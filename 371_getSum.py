dfb1079bd008ba141ecd30b096f606ccc665ffb2
Angie Yi <yih@onid.oregonstate.edu>
2016-12-18 22:25:45 -0800

    come on

--- 371_getSum.py
+++ 371_getSum.py
@@ -1,3 +1,9 @@
+'''
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+Example:
+Given a = 1 and b = 2, return 3.
+'''
+
 def getSum(self, a, b):
     MAX_INT = 0x7FFFFFFF
     MIN_INT = 0x80000000
