bd9ffb5dc509114f2158e8dda7866f4a50d254f7
AngieYi <hongyan.angie.yi@gmail.com>
2017-01-03 21:58:06 -0800

    palindrome

--- integer/9_Palindrome Number.py
+++ integer/9_Palindrome Number.py
@@ -14,4 +14,23 @@ public boolean isPalindrome(int x) {
     }
     return (x==rev || x==rev/10);
 }
+
+public boolean isPalindrome(int x) {
+
+    if (x < 0) return false;
+
+    int p = x;
+    int q = 0;
+
+    while (p >= 10){
+        q *=10;
+        q += p%10;
+        p /=10;
+    }
+
+    return q == x / 10 && p == x % 10;
+}
+// so the reversed version of int is always 1 time short in the factor of 10s .
+
+in case of Int16, check 63556 will finally check if (6553 == 6355 && 6 == 63556%10) so there will have no concerns about the overflow.
 '''
